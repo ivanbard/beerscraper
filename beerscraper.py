ba25151353e9bda68f762d8e14dd00bde981bc6a
ivanbard <ivanbardziyan@gmail.com>
2025-02-15 18:07:13 -0500

    working before splitting string

--- beerscraper.py
+++ beerscraper.py
@@ -10,14 +10,13 @@ import chromedriver_autoinstaller
 
 chromedriver_autoinstaller.install()
 
-# Configure Selenium options
+#selenium options
 options = Options()
-options.add_argument("--headless")  # Run in background (no browser pop-up)
+options.add_argument("--headless") #in background instead of popping up
 options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1920,1080")
 options.add_argument("start-maximized")
 
-# Start Chrome WebDriver
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.thebeerstore.ca/beers")
 time.sleep(5)
@@ -28,9 +27,15 @@ driver.quit()
 
 beer_data = soup.find_all('div', class_="justify-between text-card-foreground flex info-height flex-col border border-[#D6D6D6] border-solid rounded-[10px] relative shadow-none overflow-hidden clear-both")
 beer_list = [beer.get_text(strip=True) for beer in beer_data]
+'''
 if beer_list:
     print(f"Found {len(beer_list)} beers.")
-    for name in beer_list[:10]:  # Print first 10 for verification
+    for name in beer_list:
         print(name)
 else:
     print("No beer data found. Check HTML structure again.")
+'''
+
+clean_list = [name.split('*')[0].strip() for name in beer_list]
+for name in clean_list:
+    print(name)
